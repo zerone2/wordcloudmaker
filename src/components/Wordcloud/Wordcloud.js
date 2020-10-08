47098256b2f3f2b9a180bbe269e1c34c177be1fb
조영일 <zerone.cho@zipfund.co>
2020-10-08 18:59:54 +0900

    [add: controllers] add wordcloud controller

--- src/components/Wordcloud/Wordcloud.js
+++ src/components/Wordcloud/Wordcloud.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { observer } from 'mobx-react'
 import ReactWordcloud from 'react-wordcloud'
+import { useStores } from 'src/stores'
 import './wordcloud.scss'
 
-const Wordcloud = React.memo((props) => {
+const Wordcloud = React.memo(observer((props) => {
+  const { words, options } = props
+  const { width, height, maxWords, minFont, maxFont, spiral } = options
+  const { appStore: { setCloudRef }} = useStores()
+  
   const getCallback = callbackName => (word, event) => {
     const isActive = callbackName !== 'onWordMouseOut'
     const element = event.target
@@ -10,21 +16,30 @@ const Wordcloud = React.memo((props) => {
     element.setAttribute('font-size', isActive ? '300%' : '100%')
     element.setAttribute('text-decoration', isActive ? 'underline' : 'none')
   }
+  const cloudRef = useRef(null)
+  if(cloudRef.current) setCloudRef(cloudRef.current.firstChild)
+  
   return (
-    <div className={'wordcloud-container'}>
-      <ReactWordcloud words={props.words}
+    <div className={'wordcloud-container'} ref={cloudRef} id={'check'}>
+      <ReactWordcloud
+        words={words}
+        maxWords={maxWords}
+        size={[width, height]}
         options={{
           rotations: 3,
           rotationAngles: [0, 90],
+          fontSizes: [minFont, maxFont],
+          spiral: spiral
         }}
         callbacks={{
           onWordClick: getCallback('onWordClick'),
           onWordMouseOut: getCallback('onWordMouseOut'),
           onWordMouseOver: getCallback('onWordMouseOver'),
         }}
+        style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
       />
     </div>
   )
-})
+}))
 
 export default Wordcloud
